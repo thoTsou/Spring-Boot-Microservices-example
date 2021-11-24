9e566349eb319c96986212ed4b13aea6b1d53f1e
thotsou <t.tsoufis@msensis.com>
2021-11-24 12:18:37 +0200

    Push this repo to github

---
+++ department-service/src/main/kotlin/com/thoTsouDev/department/DepartmentServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.thoTsouDev.department
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DepartmentServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DepartmentServiceApplication>(*args)
+}
---
+++ department-service/src/test/kotlin/com/thoTsouDev/department/DepartmentServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.thoTsouDev.department
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
